bc2c92c07e47ee2013dc6f320ba8b3bc9684e3bc
Jack Programs <52725440+jackprogramsjp@users.noreply.github.com>
2022-03-06 01:57:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,12 @@ then
     abort "Bash is required to execute this script."
 fi
 
+# Check if `java` exists in their system
+if ! [ -x "$(command -v java)" ]
+then
+    abort "Java must be installed. You can install by here: https://java.com/en/download/help/download_options.html"
+fi
+
 # Download
 rm /usr/local/bin/scalculator /usr/local/bin/scalculator.jar # Make sure scalculator is removed before installing it with curl
 curl -f https://raw.githubusercontent.com/scalculator/scalculator/main/scalculator.jar -o /usr/local/bin/scalculator.jar && echo "Successfully installed the Scalculator JAR file." || echo "Failed to install the Scalculator JAR file with curl."
